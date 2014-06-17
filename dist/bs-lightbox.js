90b32db407fc915f0cb38a3c8c3d933e6fc372b3
Sebastian Dawidziak <sebastian@dawidziak.eu>
2014-06-17 23:45:13 +0200

    v 0.0.1

--- dist/bs-lightbox.js
+++ dist/bs-lightbox.js
@@ -1,81 +1,175 @@
 "use strict";
-var $, ModelLightbox;
+var $, Lightbox;
 
 $ = jQuery;
 
-this.modal = $('<div/>', {
-  "class": 'light-modal fade'
-});
-
-this.modal_dialog = $('<div/>', {
-  "class": 'modal-dialog'
-});
-
-this.modal_container = $('<div/>', {
-  "class": 'modal-content'
-});
-
-this.modal_head = $('<div/>', {
-  "class": 'modal-header'
-});
-
-this.modal_close_button = $('<button/>', {
-  type: 'button',
-  "class": 'close',
-  dataDismiss: 'modal'
-});
-
-this.modal_close_button.html('&times;');
-
-this.modal_head_title = $('<h4/>');
-
-this.modal_head_title.text('teest');
-
-this.modal_head.append(this.modal_close_button);
-
-this.modal_head.append(this.modal_head_title);
-
-this.modal_body = $('<div/>', {
-  "class": 'modal-body'
-});
-
-this.modal_container.append(this.modal_head);
-
-this.modal_container.append(this.modal_body);
-
-this.modal_dialog.append(this.modal_container);
-
-this.modal.append(this.modal_dialog);
-
-$('body').append(this.modal);
-
-this.modal.modal({
-  show: false,
-  keyboard: true
-});
-
-ModelLightbox = function(element, options) {
-  this.options = $.extend({
-    title: null,
-    source: null
-  }, $.fn.bsLightbox.defaults, options || {});
-  this.$element = $(element);
-  this.modal.on('show.bs.modal', this.options.onShow.bind(this)).on('shown.bs.modal', (function(_this) {
-    return function() {
-      _this.modal_shown();
-      return _this.options.onShown.call(_this);
-    };
-  })(this)).on('hide.bs.modal', this.options.onHide.bind(this)).on('hidden.bs.modal', (function(_this) {
-    return function() {
-      if (_this.gallery) {
-        $(document).off('keydown.ekkoLightbox');
-        _this.modal.remove();
+Lightbox = {
+  images: {},
+  options: {},
+  modal: null,
+  modal_title: null,
+  modal_container: null,
+  modal_setTitle: function(title) {
+    if (title != null) {
+      this.modal.find('.modal-title').show().text(title);
+    } else {
+      this.modal.find('.modal-title').hide();
+    }
+  },
+  _bind: function(element) {
+    element.click(function(event) {
+      event.preventDefault();
+      return Lightbox._open(this);
+    });
+    return this;
+  },
+  _bindGallery: function() {
+    var $images;
+    $images = this.images;
+    this.modal.find('.modal-switcher a').click(function(event) {
+      var $current, $next, $prev, $this, image, _i, _len;
+      event.preventDefault();
+      $this = $(this);
+      $current = null;
+      for (_i = 0, _len = $images.length; _i < _len; _i++) {
+        image = $images[_i];
+        if ($(image).hasClass('active') === true) {
+          $current = $(image);
+        }
       }
-      return _this.options.onHidden.call(_this);
-    };
-  })(this)).modal('show', options);
-  return this.modal;
+      $next = $images.next();
+      $prev = $images.prev();
+      if ($next.hasClass('active') === true) {
+        $next = $images.first();
+      }
+      if ($prev.hasClass('active') === true) {
+        $prev = $images.last();
+      }
+      if ($this.is('.pull-left')) {
+        if ($current != null) {
+          $current.removeClass('active').fadeOut(250, (function() {
+            return $prev.addClass('active').fadeIn(250);
+          }));
+          Lightbox.modal_setTitle($prev.attr('title'));
+        }
+      } else {
+        if ($current != null) {
+          $current.removeClass('active').fadeOut(250, (function() {
+            return $next.addClass('active').fadeIn(250);
+          }));
+          Lightbox.modal_setTitle($next.attr('title'));
+        }
+      }
+    });
+    return this;
+  },
+  _unbindGallery: function() {
+    this.modal.find('.modal-switcher a').unbind('click').off();
+    return this;
+  },
+  _create: function(source, options) {
+    this._setOptions(options);
+    return this;
+  },
+  _open: function(element, event) {
+    if (this._hasGallery(element) === true) {
+      this._makeGallery(element);
+    } else {
+      this._makeSingle(element);
+    }
+    return this;
+  },
+  _makeSingle: function(element) {
+    var img, source, title;
+    this._unbindGallery();
+    element = $(element);
+    source = element.attr('href');
+    title = element.data('title');
+    this.modal.find('.modal-switcher').hide();
+    this.modal_setTitle(title);
+    img = $('<img/>', {
+      src: source,
+      title: title,
+      alt: '',
+      width: '100%'
+    });
+    this.modal.find('.modal-images').empty().append(img);
+    this.modal.modal('show');
+    return this;
+  },
+  _makeGallery: function(element) {
+    var galleryName, image, images, img, source, src, title, _i, _len;
+    element = $(element);
+    src = element.attr('href');
+    title = element.data('title');
+    this.modal_setTitle(title);
+    galleryName = element.data('gallery');
+    images = $('[data-gallery="' + galleryName + '"]');
+    this.modal.find('.modal-images').empty();
+    for (_i = 0, _len = images.length; _i < _len; _i++) {
+      image = images[_i];
+      image = $(image);
+      source = image.attr('href');
+      title = image.data('title');
+      img = $('<img/>', {
+        src: source,
+        title: title,
+        alt: '',
+        width: '100%',
+        "class": source === src ? 'active' : void 0
+      });
+      this.modal.find('.modal-images').append(img);
+    }
+    images = this.modal.find('img');
+    images.not('.active').hide();
+    this.modal.find('.modal-switcher').show();
+    this.images = images;
+    this._bindGallery();
+    this.modal.modal('show');
+    return this;
+  },
+  _hasGallery: function(target) {
+    var elements, galleryName;
+    galleryName = $(target).data('gallery');
+    if (galleryName === void 0) {
+      return false;
+    }
+    elements = $('*[data-gallery="' + galleryName + '"]');
+    if (elements.length > 1) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  _setOptions: function(options) {
+    this.options = $.extend({
+      title: null,
+      source: null
+    }, $.fn.bsLightbox.defaults, options || {});
+    return this;
+  },
+  _makeModal: function() {
+    var template;
+    template = '<div class="lightbox-modal modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
+    template += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
+    template += '<h4 class="modal-title">title</h4>';
+    template += '</div><div class="modal-body">';
+    template += '<div class="modal-images"></div><div class="modal-switcher">';
+    template += '<a href="#" class="pull-left"><i class="glyphicon glyphicon-chevron-left"></i></a>';
+    template += '<a href="#" class="pull-right"><i class="glyphicon glyphicon-chevron-right"></i></a>';
+    template += '</div>';
+    template += '</div></div></div></div>';
+    this.modal = $(template);
+    $('body').append(this.modal);
+    this.modal.modal({
+      show: false,
+      keyboard: true
+    });
+    return this.modal;
+  }
 };
+
+Lightbox._makeModal();
 
 $.fn.bsLightbox = function(options) {
   return this.each(function() {
@@ -85,7 +179,8 @@ $.fn.bsLightbox = function(options) {
       source: $this.attr('href'),
       type: $this.attr('data-type')
     }, options, $this.data());
-    new ModelLightbox(this, options);
+    Lightbox._create(this, options);
+    Lightbox._bind($this);
     return this;
   });
 };
--- source/bs-lightbox.coffee
+++ source/bs-lightbox.coffee
@@ -2,83 +2,204 @@
 
 $ = jQuery
 
-@modal = $ '<div/>',
-	class: 'light-modal fade'
+Lightbox =
+	images: {}
+	options: {}
+	modal: null
+	modal_title: null
+	modal_container: null
+	modal_setTitle: (title) ->
+		if title?
+			@.modal
+				.find '.modal-title'
+				.show()
+				.text title
+		else			
+			@.modal
+				.find '.modal-title'
+				.hide()
+		return
 
-@modal_dialog = $ '<div/>',
-	class: 'modal-dialog'
+	_bind: (element) ->
+		element
+			.click (event) ->
+				do event.preventDefault
+				Lightbox._open this
+		return @
 
-@modal_container = $ '<div/>',
-	class: 'modal-content'
-
-@modal_head = $ '<div/>',
-	class: 'modal-header'
-
-@modal_close_button = $ '<button/>',
-	type: 'button'
-	class: 'close'
-	dataDismiss: 'modal'
-
-@modal_close_button
-	.html '&times;'
-
-@modal_head_title = $ '<h4/>'
-@modal_head_title
-	.text 'teest'
-
-@modal_head
-	.append @modal_close_button
-
-@modal_head
-	.append @modal_head_title
-
-@modal_body = $ '<div/>',
-	class: 'modal-body'
-@modal_container
-	.append @modal_head
-
-@modal_container
-	.append @modal_body
-
-@modal_dialog
-	.append @modal_container
-@modal
-	.append @modal_dialog
-
-$ 'body'
-	.append @modal
-
-@modal
-	.modal
-		show: false
-		keyboard: true
+	_bindGallery: () ->
+		$images = @.images
+		@.modal
+			.find '.modal-switcher a'
+			.click (event) ->
+				do event.preventDefault
+				$this = $ this				
+				$current = null
+				for image in $images
+					if $(image).hasClass('active') is on
+						$current = $(image)
+				$next = $images.next()
+				$prev = $images.prev()
 
 
-ModelLightbox = ( element, options ) ->
+				if $next.hasClass('active') is on
+					$next = $images.first()
+				if $prev.hasClass('active') is on
+					$prev = $images.last()
 
-	@options = $.extend({
-		title : null
-		source : null
-	}, $.fn.bsLightbox.defaults, options || {})
+				if $this.is('.pull-left')
+					if $current?
+						$current.removeClass('active').fadeOut(250, (->
+							$prev.addClass('active').fadeIn(250)
+						))
+						Lightbox.modal_setTitle $prev.attr('title')
+				else
+					if $current?
+						$current.removeClass('active').fadeOut(250, (->
+							$next.addClass('active').fadeIn(250)
+						))
+						Lightbox.modal_setTitle $next.attr('title')
 
-	@$element = $ element
+				return
+		return @
+	_unbindGallery: () ->
+		@.modal
+			.find '.modal-switcher a'
+			.unbind 'click'
+			.off()
+		return @
+
+	_create: (source, options) ->
+		@._setOptions options
+		return @
+	_open: (element, event) ->
+		if @._hasGallery(element) is on
+			@._makeGallery element
+		else
+			@._makeSingle element
+		return @
+	_makeSingle: (element) ->
+		do @._unbindGallery
+		element = $ element
+		source = element
+			.attr 'href'
+		title = element
+			.data 'title'
+
+		@.modal
+			.find '.modal-switcher'
+			.hide()
+
+		@.modal_setTitle title
+
+		img = $ '<img/>',
+			src: source
+			title: title
+			alt: ''
+			width: '100%'
+
+		@.modal
+			.find '.modal-images'
+			.empty()
+			.append img
+
+		@.modal
+			.modal 'show'
+		return @
+	_makeGallery: (element) ->
+		element = $ element
+
+		src = element
+			.attr 'href'
+		title = element
+			.data 'title'
+
+		@.modal_setTitle title
+
+		galleryName = element
+			.data 'gallery'
+
+		images = $ '[data-gallery="' + galleryName + '"]'
+
+		@.modal
+			.find '.modal-images'
+			.empty()
+
+		for image in images
+			image = $ image
+			source = image
+				.attr 'href'
+			title = image
+				.data 'title'
+			img = $ '<img/>',
+				src: source
+				title: title
+				alt: ''
+				width: '100%'
+				class: 'active' if source is src
+			@.modal
+				.find '.modal-images'
+				.append img
 
 
+		images = @.modal
+			.find 'img'
 
-	@modal
-		.on 'show.bs.modal', @options.onShow.bind(@)
-		.on 'shown.bs.modal', =>
-			@modal_shown()
-			@options.onShown.call(@)
-		.on 'hide.bs.modal', @options.onHide.bind(@)
-		.on 'hidden.bs.modal', =>
-			if @gallery
-				$ document
-					.off 'keydown.ekkoLightbox'
-				do @modal.remove
-			@options.onHidden.call @
-		.modal 'show', options
-	@modal
+		images.not('.active').hide()
+
+		@.modal
+			.find '.modal-switcher'
+			.show()
+
+		@.images = images
+
+		do @._bindGallery
+
+		@.modal
+			.modal 'show'
+
+		return @
+
+	_hasGallery: (target) ->
+		galleryName = $ target
+			.data 'gallery'
+		return false if galleryName is undefined
+
+		elements = $ '*[data-gallery="' + galleryName + '"]'
+
+		return if elements.length > 1 then true else false
+
+	_setOptions: (options) ->
+		@.options = $.extend({
+			title : null
+			source : null
+		}, $.fn.bsLightbox.defaults, options || {})
+		return @
+
+	_makeModal: () ->
+		template = '<div class="lightbox-modal modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">'
+		template += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'
+		template += '<h4 class="modal-title">title</h4>'
+		template += '</div><div class="modal-body">'
+		template += '<div class="modal-images"></div><div class="modal-switcher">'
+		template += '<a href="#" class="pull-left"><i class="glyphicon glyphicon-chevron-left"></i></a>'
+		template += '<a href="#" class="pull-right"><i class="glyphicon glyphicon-chevron-right"></i></a>'
+		template += '</div>'
+		template += '</div></div></div></div>'
+
+
+		@modal = $ template
+
+		$ 'body'
+			.append @modal
+
+		@modal
+			.modal
+				show: false
+				keyboard: true
+		return @modal
+
+do Lightbox._makeModal
 
 $.fn.bsLightbox = ( options ) ->
 	@each ->
@@ -87,7 +208,8 @@ $.fn.bsLightbox = ( options ) ->
 			source : $this.attr('href')
 			type : $this.attr('data-type')
 		}, options, $this.data())
-		new ModelLightbox(@, options)
+		Lightbox._create @, options
+		Lightbox._bind $this
 		@
 
 $.fn.bsLightbox.defaults =
